cf17c39dc4cfde13575b8523a22f66d56858d99a
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2006-08-10 19:12:57 +0000

    *** empty log message ***
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/trunk@502 73f06212-78d2-4448-adb7-01a149c5d412

--- src/fr/imag/clips/papillon/presentation/AdminInformations.java
+++ src/fr/imag/clips/papillon/presentation/AdminInformations.java
@@ -9,6 +9,9 @@
  * $Id$
  *-----------------------------------------------
  * $Log$
+ * Revision 1.9  2006/08/10 19:12:57  mangeot
+ * *** empty log message ***
+ *
  * Revision 1.8  2006/08/10 18:36:26  mangeot
  * *** empty log message ***
  *
@@ -475,7 +478,7 @@ public class AdminInformations extends PapillonBasePO {
 				// Les infos récupérées des fichiers de config sont encodées en codage local.
 				// Il faut donc les redécoder à l'envers pour avoir de l'unicode 
 				
-                theOptionTemplate.setLabel(Utility.convertFromEncoding(sectionLocalizedNames[i],System.getProperty ("file.encoding")));
+                theOptionTemplate.setLabel(Utility.convertFromEncoding(sectionLocalizedNames[i],Languages.getDefaultEncoding()));
                 // Je dois ici mettre un text dans l'OPTION, car les browser PC ne sont pas conformes aux
                 // specs W3C.
                 theTextTemplate.setData(sectionLocalizedNames[i]);
