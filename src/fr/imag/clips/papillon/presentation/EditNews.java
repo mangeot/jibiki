f8cab18e236377e936eb2e25f34eb52374ff445b
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2006-08-10 21:32:34 +0000

    *** empty log message ***
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/trunk@507 73f06212-78d2-4448-adb7-01a149c5d412

---
+++ src/fr/imag/clips/papillon/presentation/EditNews.java
@@ -0,0 +1,158 @@
+/*
+ * papillon 
+ *
+ * Enhydra super-servlet
+ * 
+ * © Mathieu Mangeot & Gilles Sérasset - GETA CLIPS IMAG
+ * Projet Papillon
+ *-----------------------------------------------
+ * $Id$
+ *-----------------------------------------------
+ * $Log$
+ * Revision 1.1  2006/08/10 21:28:29  mangeot
+ * *** empty log message ***
+ *
+ * Revision 1.1  2005/07/28 14:36:56  mangeot
+ * Added a News presentation page that takes a static xhtml page and displays it.
+ * People can edit this static page on the server by accessing it via ftp or ssh.
+ * There is a cache that can be reset via the Admin.po page.
+ *
+ *
+ *-----------------------------------------------
+ * Papillon News page.
+ */
+
+package fr.imag.clips.papillon.presentation;
+
+// Enhydra SuperServlet imports
+import com.lutris.appserver.server.httpPresentation.HttpPresentation;
+import com.lutris.appserver.server.httpPresentation.HttpPresentationException;
+
+import fr.imag.clips.papillon.presentation.xhtml.orig.EditNewsTmplXHTML;
+import fr.imag.clips.papillon.business.utility.Utility;
+
+
+public class EditNews extends PapillonBasePO {
+	
+    protected final static String BASE_DIR_CONFIG = "Papillon.Informations.baseDir";
+    protected final static String MEDIA_DIR_CONFIG = "Papillon.Informations.mediaDir";
+	protected final static String NEWS_VOLUME_DIR="news";
+	protected final static String NEWS_FILE="news.xhtml";
+	protected final static String NewsContentIdString = "NewsContent";
+
+    protected boolean loggedInUserRequired() {
+        return false;
+    }
+
+    protected boolean userMayUseThisPO() {
+        return true;
+    }
+
+    protected  int getCurrentSection() {
+        return NO_SECTION;
+    }
+
+
+    public org.w3c.dom.Node getContent()
+    throws HttpPresentationException, java.io.IOException {
+		org.w3c.dom.Node NewsDOMCache = null;
+		String submit = myGetParameter(EditNewsTmplXHTML.NAME_submit);
+		String textarea = myGetParameter(EditNewsTmplXHTML.NAME_Textarea);
+		
+		if (submit != null && !submit.equals("")
+			&& textarea != null && !textarea.equals("")) {
+			
+			textarea = textarea.substring(textarea.indexOf("<?xml version"));
+			
+			org.w3c.dom.Document newDocument = Utility.buildDOMTree(textarea);
+			//org.w3c.dom.Element newElement = newDocument.getElementById(NewsContentIdString);
+			org.w3c.dom.Element newElement = newDocument.getDocumentElement();
+			
+			org.xml.sax.InputSource newsInputSource = getInputSource(getNewsFileAbsolutePath());
+			if (newsInputSource != null) {
+				org.w3c.dom.Document myNewsDocument = fr.imag.clips.papillon.business.utility.Utility.buildDOMTree(newsInputSource);
+				if (myNewsDocument != null) {
+					NewsDOMCache = myNewsDocument.getElementById(NewsContentIdString);
+					org.w3c.dom.Node newParentNode = NewsDOMCache.getParentNode();
+					org.w3c.dom.Node importedNode = myNewsDocument.importNode((org.w3c.dom.Node)newElement, true);
+					newParentNode.replaceChild(importedNode,(org.w3c.dom.Node)NewsDOMCache);
+				}
+				java.io.OutputStream outStream = new java.io.FileOutputStream(getNewsFileAbsolutePath());
+				java.io.PrintStream myPrintStream = new java.io.PrintStream(outStream);
+				myPrintStream.print(Utility.NodeToString(myNewsDocument,true,true,true));
+				myPrintStream.close();
+				outStream.close();
+			}
+		}
+		
+		EditNewsTmplXHTML content = (EditNewsTmplXHTML)MultilingualXHtmlTemplateFactory.createTemplate("EditNewsTmplXHTML", this.getComms(), this.getSessionData());
+		
+			org.xml.sax.InputSource newsInputSource = getInputSource(getNewsFileAbsolutePath());
+			if (newsInputSource != null) {
+				org.w3c.dom.Document myNewsDocument = fr.imag.clips.papillon.business.utility.Utility.buildDOMTree(newsInputSource);
+				if (myNewsDocument != null) {
+					NewsDOMCache = myNewsDocument.getElementById(NewsContentIdString);
+				}
+			}
+			
+		org.w3c.dom.Text textareaTextNode = new org.apache.xerces.dom.TextImpl((org.apache.xerces.dom.DocumentImpl)content.getOwnerDocument(), Utility.NodeToString((org.w3c.dom.Element) NewsDOMCache));
+
+        //On rend le contenu correct
+		content.getElementPreview().appendChild(content.importNode(NewsDOMCache, true));
+		content.getElementTextarea().appendChild(content.importNode(textareaTextNode, true));
+		
+		return content.getElementInfoContent();
+
+    }
+	
+	protected String getNewsFileAbsolutePath() throws PapillonPresentationException {            
+		String baseDir = "";
+		String mediaDir = "";
+		String filePath = "";
+		try {
+			baseDir = com.lutris.appserver.server.Enhydra.getApplication().getConfig().getString(BASE_DIR_CONFIG);
+			mediaDir = com.lutris.appserver.server.Enhydra.getApplication().getConfig().getString(MEDIA_DIR_CONFIG);
+			if (! baseDir.endsWith(java.io.File.separator)) {
+				baseDir = baseDir + java.io.File.separator;
+			}
+			if (! mediaDir.endsWith(java.io.File.separator)) {
+				mediaDir = mediaDir + java.io.File.separator;
+			}
+		}
+		catch (com.lutris.util.ConfigException ex) {
+			throw new PapillonPresentationException("Error in Papillon Configuration File: ", ex);
+		}
+		filePath = baseDir + mediaDir + NEWS_VOLUME_DIR + java.io.File.separator + NEWS_FILE;
+		return filePath;
+	}
+	
+	protected org.xml.sax.InputSource getInputSource(String filePath) 
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {	
+			org.xml.sax.InputSource myInputSource = null;
+			try {
+				java.io.FileInputStream myFileInputStream = new java.io.FileInputStream(filePath);
+				myInputSource = new org.xml.sax.InputSource(myFileInputStream);
+			}
+			catch(java.io.IOException ioex) {
+				throw new fr.imag.clips.papillon.business.PapillonBusinessException("Exception in renderOutputStream()", ioex);
+			}
+			return myInputSource;
+		}
+	
+	// Copies src file to dst file.
+	   // If the dst file does not exist, it is created
+    protected static void copyFile(java.io.File src, java.io.File dst) throws java.io.IOException {
+        java.io.InputStream in = new java.io.FileInputStream(src);
+        java.io.OutputStream out = new java.io.FileOutputStream(dst);
+		
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+	
+}
