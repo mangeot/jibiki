cbdf1d0f84f534f9ae02640e5623576fb93f6b4b
serasset <serasset@73f06212-78d2-4448-adb7-01a149c5d412>
2007-11-14 16:36:04 +0000

    Added italian sorting to SQL sort functions.
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/XALAN27_NEWDISPLAY@729 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: 384b7826b56b490bcf7cb29cb2339295647aef44

--- src/sql/multilingual-sort.sql
+++ src/sql/multilingual-sort.sql
@@ -591,20 +591,223 @@ CREATE OR REPLACE FUNCTION fra_sort( varchar )
 $PROC$ LANGUAGE 'plpgsql' WITH ( ISCACHABLE );
 
 -- ita Italian
---
--- for the moment do nothing
--- todo: maybe italian sort 
-CREATE OR REPLACE FUNCTION ita_sort( varchar ) 
- RETURNS varchar AS '
+-- (a à) b c d (e é è) f g h (i ï ì í) j k l m n (o ò ó Ó Ò) p q r s t (u Ù ù Ú ú) v w x y z
+
+CREATE OR REPLACE FUNCTION ita_sort( varchar )
+ RETURNS varchar AS $PROC$
 
  DECLARE
+ i		integer;
   tmp		char;
-  result	varchar := '''';
+  result	varchar := '';
   length	integer;
  BEGIN
-  return( $1 );
+ 	length := char_length($1);
+  	FOR i IN 1.. length LOOP
+  		tmp := SUBSTR( $1, i, 1 );
+  IF tmp = 'A'  THEN
+    result:= result || '01';
+  ELSIF tmp = 'a'  THEN
+    result:= result || '01';
+  ELSIF tmp = 'À' THEN
+    result:= result || '01';
+  ELSIF tmp = 'à' THEN
+    result:= result || '01';
+  ELSIF tmp = 'Â' THEN
+    result:= result || '01';
+  ELSIF tmp = 'â' THEN
+    result:= result || '01';
+  ELSIF tmp = 'Ä' THEN
+    result:= result || '01';
+  ELSIF tmp = 'ä' THEN
+    result:= result || '01';
+  ELSIF tmp = 'Á' THEN
+    result:= result || '01';
+  ELSIF tmp = 'á' THEN
+    result:= result || '01';
+  ELSIF tmp = 'B' THEN
+    result:= result || '02';
+  ELSIF tmp = 'b' THEN
+    result:= result || '02';
+  ELSIF tmp = 'C' THEN
+    result:= result || '03';
+  ELSIF tmp = 'c' THEN
+    result:= result || '03';
+  ELSIF tmp = 'Ç' THEN
+    result:= result || '03';
+  ELSIF tmp = 'ç' THEN
+    result:= result || '03';
+  ELSIF tmp = 'D' THEN
+    result:= result || '04';
+  ELSIF tmp = 'd' THEN
+    result:= result || '04';
+  ELSIF tmp = 'E' THEN
+    result:= result || '05';
+  ELSIF tmp = 'e' THEN
+    result:= result || '05';
+  ELSIF tmp = 'É' THEN
+    result:= result || '05';
+  ELSIF tmp = 'é' THEN
+    result:= result || '05';
+  ELSIF tmp = 'È' THEN
+    result:= result || '05';
+  ELSIF tmp = 'è' THEN
+    result:= result || '05';
+  ELSIF tmp = 'Ê' THEN
+    result:= result || '05';
+  ELSIF tmp = 'ê' THEN
+    result:= result || '05';
+  ELSIF tmp = 'Ë' THEN
+    result:= result || '05';
+  ELSIF tmp = 'ë' THEN
+    result:= result || '05';
+  ELSIF tmp = 'F' THEN
+    result:= result || '06';
+  ELSIF tmp = 'f' THEN
+    result:= result || '06';
+  ELSIF tmp = 'G' THEN
+    result:= result || '07';
+  ELSIF tmp = 'g' THEN
+    result:= result || '07';
+  ELSIF tmp = 'H' THEN
+    result:= result || '08';
+  ELSIF tmp = 'h' THEN
+    result:= result || '08';
+  ELSIF tmp = 'I' THEN
+    result:= result || '09';
+  ELSIF tmp = 'i' THEN
+    result:= result || '09';
+  ELSIF tmp = 'Î' THEN
+    result:= result || '09';
+  ELSIF tmp = 'î' THEN
+    result:= result || '09';
+  ELSIF tmp = 'Ï' THEN
+    result:= result || '09';
+  ELSIF tmp = 'ï' THEN
+    result:= result || '09';
+  ELSIF tmp = 'Ì' THEN
+    result:= result || '09';
+  ELSIF tmp = 'ì' THEN
+    result:= result || '09';
+  ELSIF tmp = 'Í' THEN
+    result:= result || '09';
+  ELSIF tmp = 'í' THEN
+    result:= result || '09';
+  ELSIF tmp = 'J' THEN
+    result:= result || '10';
+  ELSIF tmp = 'j' THEN
+    result:= result || '10';
+  ELSIF tmp = 'K' THEN
+    result:= result || '11';
+  ELSIF tmp = 'k' THEN
+    result:= result || '11';
+  ELSIF tmp = 'L' THEN
+    result:= result || '12';
+  ELSIF tmp = 'l' THEN
+    result:= result || '12';
+  ELSIF tmp = 'M' THEN
+    result:= result || '13';
+  ELSIF tmp = 'm' THEN
+    result:= result || '13';
+  ELSIF tmp = 'N' THEN
+    result:= result || '14';
+  ELSIF tmp = 'n' THEN
+    result:= result || '14';
+  ELSIF tmp = 'O' THEN
+    result:= result || '15';
+  ELSIF tmp = 'o' THEN
+    result:= result || '15';
+  ELSIF tmp = 'Ö' THEN
+    result:= result || '15';
+  ELSIF tmp = 'ö' THEN
+    result:= result || '15';
+  ELSIF tmp = 'ô' THEN
+    result:= result || '15';
+  ELSIF tmp = 'Ô' THEN
+    result:= result || '15';
+  ELSIF tmp = 'Ó' THEN
+    result:= result || '15';
+  ELSIF tmp = 'ó' THEN
+    result:= result || '15';
+  ELSIF tmp = 'Ò' THEN
+    result:= result || '15';
+  ELSIF tmp = 'ò' THEN
+    result:= result || '15';
+  ELSIF tmp = 'p' THEN
+    result:= result || '16';
+  ELSIF tmp = 'P' THEN
+    result:= result || '16';
+  ELSIF tmp = 'Q' THEN
+    result:= result || '17';
+  ELSIF tmp = 'q' THEN
+    result:= result || '17';
+  ELSIF tmp = 'R' THEN
+    result:= result || '18';
+  ELSIF tmp = 'r' THEN
+    result:= result || '18';
+  ELSIF tmp = 'S' THEN
+    result:= result || '19';
+  ELSIF tmp = 's' THEN
+    result:= result || '19';
+  ELSIF tmp = 'T' THEN
+    result:= result || '20';
+  ELSIF tmp = 't' THEN
+    result:= result || '20';
+  ELSIF tmp = 'U' THEN
+    result:= result || '22';
+  ELSIF tmp = 'u' THEN
+    result:= result || '22';
+  ELSIF tmp = 'Ù' THEN
+    result:= result || '22';
+  ELSIF tmp = 'ù' THEN
+    result:= result || '22';
+  ELSIF tmp = 'Û' THEN
+    result:= result || '22';
+  ELSIF tmp = 'û' THEN
+    result:= result || '22';
+  ELSIF tmp = 'Ü' THEN
+    result:= result || '22';
+  ELSIF tmp = 'ü' THEN
+    result:= result || '22';
+  ELSIF tmp = 'Ú' THEN
+    result:= result || '22';
+  ELSIF tmp = 'ú' THEN
+    result:= result || '22';
+  ELSIF tmp = 'V' THEN
+    result:= result || '23';
+  ELSIF tmp = 'v' THEN
+    result:= result || '23';
+  ELSIF tmp = 'W' THEN
+    result:= result || '24';
+  ELSIF tmp = 'w' THEN
+    result:= result || '24';
+  ELSIF tmp = 'X' THEN
+    result:= result || '25';
+  ELSIF tmp = 'x' THEN
+    result:= result || '25';
+  ELSIF tmp = 'Y' THEN
+    result:= result || '26';
+  ELSIF tmp = 'y' THEN
+    result:= result || '26';
+  ELSIF tmp = 'Z' THEN
+    result:= result || '27';
+  ELSIF tmp = 'z' THEN
+    result:= result || '27';
+  ELSIF tmp = ' ' THEN
+    result:= result;
+  ELSIF tmp = '-' THEN
+    result:= result;
+  ELSIF tmp = '+' THEN
+    result:= result;
+  ELSIF tmp = '#' THEN
+    result:= result;
+  ELSE
+    result:= result || '99';
+  END IF;
+	END LOOP;
+  return( result );
  END;
-' LANGUAGE 'plpgsql' WITH ( ISCACHABLE );
+$PROC$ LANGUAGE 'plpgsql' WITH ( ISCACHABLE );
 
 -- eng English
 --
