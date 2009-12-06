a44030b5fb75e8e03bed59b666bbee5a8c73316a
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2009-12-06 19:00:21 +0000

    added
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/trunk@1091 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: ab0ea594e9dcee614953e717a2c080c97d63e59d

---
+++ src/fr/imag/clips/papillon/presentation/EditEntryInit.java
@@ -0,0 +1,340 @@
+/*
+ *  Jibiki
+ *
+ *  Enhydra super-servlet presentation object
+ *
+ * © Francis Brunet-Manquat et Mathieu Mangeot - GETA CLIPS IMAG
+ * Projet Papillon
+ *
+ *-----------------------------------------------
+ * $Id: LexalpEditEntryInit.java 642 2007-04-10 14:51:14Z serasset $
+ *-----------------------------------------------
+ * $Log$
+ * Revision 1.15  2007/04/10 14:51:14  serasset
+ * LexalpEditentryInit now searches for lexies AND translation, with NO TARGET LANGUAGE selected.
+ * This way, the axie is retrieved in the result and may be linked from the lexie.
+ *
+ * Revision 1.14  2007/03/25 22:00:57  fbrunet
+ * improved avancedqueryform javascript
+ * bug correction: in ViewQueryResult class, encode url criteria in UTF-8
+ *
+ * Revision 1.13  2007/02/07 13:58:57  fbrunet
+ * added message before axies are merged and undo process if the merge is not correct.
+ *
+ * Revision 1.12  2007/01/08 15:13:42  fbrunet
+ * Correction of th xml attribut bug in ContributionHeader (VolumeEntry class)
+ *
+ * Revision 1.11  2006/09/11 19:57:48  fbrunet
+ * - bug correction : interface edition (link axie to another axi)
+ *
+ * Revision 1.10  2006/08/10 22:17:13  fbrunet
+ * - Add caches to manage Dictionaries, Volumes and Xsl sheets (improve efficiency)
+ * - Add export contibutions to pdf file base on exportVolume class and, Saxon8b & FOP transformations (modify papillon.properties to specify XML to FO xsl)
+ * - Bug correction : +/- in advanced search
+ *
+ * Revision 1.9  2006/06/19 15:27:01  fbrunet
+ * Jibiki : improvement of the search result display
+ * Lexalp : add help menu (link to wiki and bug tracker)
+ *
+ * Revision 1.8  2006/06/01 22:05:05  fbrunet
+ * New interface, quick search, new contribution management (the first edition not create new contribution. New contribution is created after add, remove element, update, save, etc. in the interface window)
+ *
+ * Revision 1.7  2006/05/05 02:08:23  fbrunet
+ * bug correction : url utf8 transfert (in createEntryInit)
+ *
+ * Revision 1.6  2006/04/24 13:43:29  fbrunet
+ * Add new class ViewQueryResult : allow to use one class to create result display in advancedSearch and EditEntryInit (like advancedQueryForm)
+ * Improve result display : view n results per page
+ *
+ * Revision 1.5  2006/04/18 14:30:24  fbrunet
+ * Authorize admin to edit all entries
+ *
+ * Revision 1.4  2006/04/06 15:06:39  fbrunet
+ * New class 'creationEditInit' : create new entry
+ * Modify LexALPEditEntry : only edit entry
+ *
+ * Revision 1.3  2006/03/13 08:48:00  fbrunet
+ * bug corrections before merge
+ *
+ * Revision 1.2  2006/03/01 15:12:31  mangeot
+ * Merge between maintrunk and LEXALP_1_1 branch
+ *
+ * Revision 1.1.4.6  2006/02/17 10:41:49  fbrunet
+ * Change QueryCriteria parameters
+ * Add new windows when editing an entry
+ *
+ * Revision 1.1.4.5  2006/01/25 16:51:46  fbrunet
+ * *** empty log message ***
+ *
+ * Revision 1.1.4.4  2006/01/25 15:22:23  fbrunet
+ * Improvement of QueryRequest
+ * Add new search criteria
+ * Add modified status
+ *
+ * Revision 1.1.4.3  2006/01/24 13:39:49  fbrunet
+ * Modification view management
+ * Modification LexALP postprocessing
+ *
+ * Revision 1.1.4.2  2005/12/02 10:04:09  fbrunet
+ * Add Pre/Post edition processing
+ * Add index reconstruction
+ * Add new query request
+ * Add fuzzy search
+ * Add new contribution administration
+ * Add xsl transformation volume
+ *
+ * Revision 1.1.4.1  2005/08/31 15:01:39  serasset
+ * Applied modifications done on the LEXALP_1_0 branch to updated sources of the
+ * trunk to create a new updated LEXALP_1_1 branch.
+ *
+ * Revision 1.1.2.1  2005/07/22 13:28:32  serasset
+ * Modified EditEntryInit for Lexalp. It now serves as a main page for db maintenance.
+ * Added a function to get url for QueryParameter.
+ * Modified the way xslsheets are handled in order to allow several xslsheet with the same name, different dicts.
+ *
+ *
+ */
+
+package fr.imag.clips.papillon.presentation;
+
+
+//General java imports
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.ArrayList;
+
+// DOM imports
+import org.w3c.dom.html.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.enhydra.xml.xhtml.dom.*;
+
+// Enhydra SuperServlet imports
+import com.lutris.appserver.server.httpPresentation.HttpPresentation;
+import com.lutris.appserver.server.httpPresentation.HttpPresentationRequest;
+import com.lutris.appserver.server.httpPresentation.HttpPresentationException;
+import com.lutris.appserver.server.httpPresentation.ClientPageRedirectException;
+
+import com.lutris.dods.builder.generator.query.QueryBuilder;
+
+// HTML source import
+import fr.imag.clips.papillon.presentation.xhtml.orig.*;
+
+//local imports
+import fr.imag.clips.papillon.business.PapillonBusinessException;
+import fr.imag.clips.papillon.business.PapillonLogger;
+import fr.imag.clips.papillon.business.dictionary.Dictionary;
+import fr.imag.clips.papillon.business.dictionary.DictionariesFactory;
+import fr.imag.clips.papillon.business.dictionary.Volume;
+import fr.imag.clips.papillon.business.dictionary.VolumesFactory;
+import fr.imag.clips.papillon.business.dictionary.VolumeEntry;
+import fr.imag.clips.papillon.business.dictionary.VolumeEntriesFactory;
+import fr.imag.clips.papillon.business.dictionary.QueryResult;
+import fr.imag.clips.papillon.business.dictionary.QueryRequest;
+import fr.imag.clips.papillon.business.dictionary.QueryCriteria;
+import fr.imag.clips.papillon.business.dictionary.QueryParameter;
+import fr.imag.clips.papillon.business.dictionary.IQuery;
+import fr.imag.clips.papillon.business.dictionary.PapillonPivotFactory;
+import fr.imag.clips.papillon.business.user.User;
+import fr.imag.clips.papillon.business.user.Group;
+import fr.imag.clips.papillon.business.utility.Utility;
+import fr.imag.clips.papillon.business.xsl.XslSheet;
+import fr.imag.clips.papillon.business.xsl.XslSheetFactory;
+import fr.imag.clips.papillon.business.transformation.ResultFormatter;
+import fr.imag.clips.papillon.business.transformation.ResultFormatterFactory;
+
+
+
+public class EditEntryInit extends PapillonBasePO {
+    
+    protected final static String HANDLE_PARAMETER = "handle";
+    protected final static String ACTION_PARAMETER = "action";
+	
+    protected final static String ContributionsURL = "AdminContributions.po";
+    protected final static String ContributionsVolumeParameter = "VOLUME";
+    
+    protected final static String ConsultExpertURL = "AdvancedSearch.po";
+    protected final static String ConsultExpertVolumeParameter = "VOLUME";
+    protected final static String ConsultExpertHandleParameter = "handle";
+    protected final static String ConsultExpertFormatterParameter = "xslid";
+    
+    protected final static String EditingErrorURL = "EditingError.po";
+    
+    protected EditEntryInitXHTML content;
+    
+    protected boolean loggedInUserRequired() {
+        return true;
+    }
+    
+    protected boolean userMayUseThisPO() {
+        return true;
+    }
+    
+    protected  int getCurrentSection() {
+        return NO_SECTION;
+    }
+    
+    public Node getContent()
+        throws HttpPresentationException,
+        java.io.UnsupportedEncodingException,
+        java.io.IOException,
+        fr.imag.clips.papillon.business.PapillonBusinessException {
+                      
+            // Content creation
+            content = (EditEntryInitXHTML)MultilingualXHtmlTemplateFactory.createTemplate("EditEntryInitXHTML", this.getComms(), this.getSessionData());
+            // On regarde d'abord les parametres qui nous sont demandes.
+            String volumeName = myGetParameter(EditEntryInitFactory.VOLUME_PARAMETER);
+            String headword = myGetParameter(content.NAME_Headword);
+            String entryHandle = myGetParameter(EditEntryInitFactory.HANDLE_PARAMETER);
+            String action = myGetParameter(EditEntryInitFactory.ACTION_PARAMETER);		
+            
+            //
+            if (volumeName != null && !volumeName.equals("")) {
+                this.setPreference(content.NAME_VOLUME, volumeName);
+            } else {
+                volumeName = this.getPreference(content.NAME_VOLUME);
+            }
+            
+            
+            //
+            if (action!=null && !action.equals("") && !action.equals("lookup") &&
+                volumeName!=null && !volumeName.equals("") &&
+                entryHandle!=null && !entryHandle.equals("")) {
+                
+                // Search last contribution corresponding to entry handle
+                VolumeEntry myEntry = VolumeEntriesFactory.findEntryByHandle(volumeName, entryHandle);
+                
+                // CREATE NEW ENTRY
+                if (action.equals("createAnyway")) {
+                    if (headword!=null && !headword.equals("")) {
+                        
+                        //
+                        EditEntryInitFactory.createEntry(volumeName, headword, this.getUser());
+                        
+                    } else {
+                        
+                        // Error message
+                        this.getSessionData().writeUserMessage("Headwords are mandatory for creation");
+                    }
+                
+                // EDIT
+                } else if (action.equals("edit")) {
+                    
+                    //
+                     EditEntryInitFactory.editEntry(myEntry, this.getUser());
+                
+                // DUPLICATE
+                } else if (action.equals("duplicate")) {
+                    
+                   //
+                   EditEntryInitFactory.duplicateEntry(myEntry, this.getUser());                    
+                    
+                // DELETE
+                } else if (action.equals("delete")) {
+                   
+                    //
+                    EditEntryInitFactory.deleteEntry(myEntry, this.getUser()); 
+                    
+                
+                // UNDELETE
+                } else if (action.equals("undelete")) {
+                   
+                    //
+                    EditEntryInitFactory.undeleteEntry(myEntry, this.getUser());
+                }      
+            }
+            
+            //
+            updateCreationInterface(volumeName, headword);
+            User myUser = getUser();
+            if (null != myUser) {
+                content.setTextUserName(myUser.getName());
+            }
+            // Do query !
+            AdvancedQueryForm qf = new AdvancedQueryForm(this.getComms(), this.getSessionData(), false, true);
+            
+			////// Display query result
+			// Create query request
+			QueryRequest queryReq = qf.getQueryRequest();
+			
+			
+			
+			// Display query result if query request have criteria
+			// Display query result if no action on form (add ou remove criteria)
+			if (!queryReq.isEmpty() && !qf.actionOnFormRequested()) {
+				
+				// Add status criteria
+				ArrayList listStatus = new ArrayList();
+				
+	/*			QueryCriteria criteriaStatus = new QueryCriteria();
+				criteriaStatus.add("key", QueryCriteria.EQUAL, Volume.CDM_contributionStatus);
+				criteriaStatus.add("value", QueryCriteria.NOT_EQUAL, VolumeEntry.CLASSIFIED_FINISHED_STATUS);
+				criteriaStatus.add("value", QueryCriteria.NOT_EQUAL, VolumeEntry.CLASSIFIED_NOT_FINISHED_STATUS);
+				criteriaStatus.add("value", QueryCriteria.NOT_EQUAL, VolumeEntry.DELETED_STATUS);
+				criteriaStatus.add("lang", QueryCriteria.EQUAL, Volume.DEFAULT_LANG);
+				listStatus.add(criteriaStatus);
+				
+				queryReq.addOrCriteriaList(listStatus); */
+				
+				// Perform the request
+				Collection qrset = queryReq.findLexieAndTranslation(this.getUser());
+				
+				// Display result
+				XHTMLElement queryResultForm = content.getElementQueryResultForm();
+				//FIXME: user is identified -> (this.getUser()!=null && !this.getUser().isEmpty())
+				// - create a method isIdentify ? create a guest user ? introduce in createNodeResult ?
+				Node viewQueryResultNode = ViewQueryResult.createNodeResult(this.getComms(), this.getSessionData(), this.getUrl(), this.getUser(), qrset, qf.getQueryParameter(), (this.getUser()!=null && !this.getUser().isEmpty()));
+				queryResultForm.appendChild(content.importNode(viewQueryResultNode, true));
+				queryResultForm.removeAttribute("id");
+			} 
+                    
+                //
+                XHTMLElement formHolder = content.getElementQueryForm();
+                formHolder.appendChild(content.importNode(qf.getQueryFormNode("EditEntryInit.po"), true));
+                
+                //
+                return content.getElementEditEntryInitContent();
+        }
+    
+    protected void updateCreationInterface(String volumeName, String headword)
+        throws fr.imag.clips.papillon.business.PapillonBusinessException,
+        HttpPresentationException,
+        java.io.UnsupportedEncodingException {
+            
+            // Addin the headword
+            HTMLInputElement headwordInputElt = content.getElementHeadword();
+            headwordInputElt.setValue(headword);
+            
+            // Adding the appropriate source languages to the source list
+            HTMLOptionElement volumeOptionTemplate = content.getElementVolumeOptionTemplate();
+            Node volumeSelect = volumeOptionTemplate.getParentNode();
+            volumeOptionTemplate.removeAttribute("id");
+            // We assume that the option element has only one text child
+            // (it should be this way if the HTML is valid...)
+            Text volumeTextTemplate = (Text)volumeOptionTemplate.getFirstChild();
+            
+            //
+            for (Iterator iter = VolumesFactory.getVolumesArray().iterator(); iter.hasNext();) {
+                Volume myVolume = (Volume)iter.next();
+                
+                //
+                String itf = myVolume.getTemplateInterface();
+                // FIXME: trick to avoid displaying Papillon axies...
+                if (itf != null && !itf.equals("") && !myVolume.getName().equals(PapillonPivotFactory.VOLUMENAME)) {
+                    volumeOptionTemplate.setValue(myVolume.getName());
+                    volumeOptionTemplate.setLabel(myVolume.getName());
+                    // Je dois ici mettre un text dans l'OPTION, car les browser PC ne sont pas conformes aux
+                    // specs W3C.
+                    volumeOptionTemplate.setSelected(myVolume.getName().equals(volumeName));
+                    volumeTextTemplate.setData(myVolume.getName());
+                    volumeSelect.appendChild(volumeOptionTemplate.cloneNode(true));
+                }
+            }
+            volumeSelect.removeChild(volumeOptionTemplate);
+        }
+}
+
