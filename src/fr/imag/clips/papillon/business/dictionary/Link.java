e8df8b2e3d0e2c8bac14bd964265875b4b9023c6
zhangying <zhangying@73f06212-78d2-4448-adb7-01a149c5d412>
2012-04-18 13:48:32 +0000

    task : 437
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/LINKS_1_0@1307 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: 3dd2c0099e4d735c01f17597955b612d180f6ac4

---
+++ src/fr/imag/clips/papillon/business/dictionary/Link.java
@@ -0,0 +1,313 @@
+/*
+ *  papillon
+ *
+ *  Enhydra super-servlet presentation object
+ * 
+ *-----------------------------------------------
+ * $Id: Link.java 1017 2012-03-26 21:00:00 Zhang ying $
+ *-----------------------------------------------
+ * $Log$
+ * 
+ *-----------------------------------------------
+ * 
+ */
+
+package fr.imag.clips.papillon.business.dictionary;
+
+import com.lutris.appserver.server.sql.DatabaseManagerException;
+import com.lutris.appserver.server.sql.ObjectIdException;
+import com.lutris.dods.builder.generator.query.DataObjectException;
+import fr.imag.clips.papillon.CurrentDBTransaction;
+import fr.imag.clips.papillon.business.PapillonBusinessException;
+import fr.imag.clips.papillon.data.LinkDO;
+
+/**
+ * Represents a volume index. 
+ */
+public class Link {
+	// Beware, for the text database columns, there is an UTF-8 conversion needed
+	// but for the character columns, no need !
+    
+    /**
+     * The DO of the Dictionary.
+     */
+    
+    protected LinkDO myDO = null;
+
+    /**
+     * The public constructor.
+     * Should find a better method instead of these if elsif elsif
+     * How to do it ?
+     */
+
+    public Link(String tableName) throws PapillonBusinessException {
+        try {
+            this.myDO = LinkDO.createVirgin(tableName, CurrentDBTransaction.get());
+        }
+        catch(DatabaseManagerException ex) {
+            throw new PapillonBusinessException("Error creating empty index", ex);
+        } catch(ObjectIdException ex) {
+            throw new PapillonBusinessException("Error creating empty index", ex);
+        }
+    }
+
+    /** The protected constructor
+     *
+     * @param theDisc. The data object of the Volume.
+     */
+    protected Link(LinkDO theIndex) 
+        throws PapillonBusinessException  {
+        this.myDO = theIndex;
+    }
+
+    public boolean isEmpty() {
+        return (this.myDO==null) ;
+    }
+
+
+    /**
+     * Gets the object id for the Dictionary
+     *
+     * @return the object id.
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+     *   error).
+     */
+    public String getHandle()
+        throws PapillonBusinessException {
+        try {
+            return this.myDO.getHandle();
+        } 
+        catch(DatabaseManagerException ex) {
+            throw new PapillonBusinessException("Error getting index's handle", ex);
+        }
+    }
+
+    /**
+     * Gets the subject of the Dictionary
+     *
+     * @return the subject.
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+     *   error).
+     */
+    public String getTableName() {
+        return this.myDO.getTableName(); 
+    }
+
+   
+   public String getTargetId()
+    throws PapillonBusinessException {
+		try {
+			return myDO.getTargetId();
+		} catch(DataObjectException ex) {
+			throw new PapillonBusinessException("Error getting target id", ex);
+		}
+	}
+
+
+
+	
+	public void setTargetId(String idTarget)
+	    throws PapillonBusinessException {
+	        try {
+	            myDO.setTargetId(idTarget);
+	        } catch(DataObjectException ex) {
+	            throw new PapillonBusinessException("Error setting index lang", ex);
+	        }
+	    }
+	
+	
+    public String getVolumeTarget()
+    throws PapillonBusinessException {
+		try {
+			return myDO.getVolumeTarget();
+		} catch(DataObjectException ex) {
+			throw new PapillonBusinessException("Error getting target volume", ex);
+		}
+	}
+
+	/**
+
+	 */
+	public void setVolumeTarget(String volume)
+	    throws PapillonBusinessException {
+	        try {
+	            myDO.setVolumeTarget(volume);
+	        } catch(DataObjectException ex) {
+	            throw new PapillonBusinessException("Error setting target volume", ex);
+	        }
+	    }
+	
+    public String getType()
+    throws PapillonBusinessException {
+		try {
+			return myDO.getType();
+		} catch(DataObjectException ex) {
+			throw new PapillonBusinessException("Error getting type", ex);
+		}
+	}
+
+	/**
+
+	 */
+	public void setType(String type)
+	    throws PapillonBusinessException {
+	        try {
+	            myDO.setType(type);
+	        } catch(DataObjectException ex) {
+	            throw new PapillonBusinessException("Error setting type", ex);
+	        }
+	    }
+	
+    public String getLabel()
+    throws PapillonBusinessException {
+		try {
+			return myDO.getLabel();
+		} catch(DataObjectException ex) {
+			throw new PapillonBusinessException("Error getting label", ex);
+		}
+	}
+
+	/**
+
+	 */
+	public void setLabel(String label)
+	    throws PapillonBusinessException {
+	        try {
+	            myDO.setLabel(label);
+	        } catch(DataObjectException ex) {
+	            throw new PapillonBusinessException("Error setting label", ex);
+	        }
+	    }
+	
+    public double getWeight()
+    throws PapillonBusinessException {
+		try {
+			return myDO.getWeight();
+		} catch(DataObjectException ex) {
+			throw new PapillonBusinessException("Error getting weight", ex);
+		}
+	}
+
+	/**
+
+	 */
+	public void setWeight(double weight)
+	    throws PapillonBusinessException {
+	        try {
+	            myDO.setWeight(weight);
+	        } catch(DataObjectException ex) {
+	            throw new PapillonBusinessException("Error setting weight", ex);
+	        }
+	    }
+	
+    /**
+		* Gets the language of the key
+     *
+     * @return the language as an ISO-639/2T 3 letters language code (fra, deu, eng, etc.).
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+						  *   error).
+     */
+	public String getLang()
+        throws PapillonBusinessException {
+			try {
+				return myDO.getLang();
+			} catch(DataObjectException ex) {
+				throw new PapillonBusinessException("Error getting index's lang", ex);
+			}
+		}
+
+    /**
+		* Sets the language of the key
+     *
+     * @param the language as an ISO-639/2T 3 letters language code (fra, deu, eng, etc.).
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+	 *   error).
+     */
+    public void setLang(String lang)
+        throws PapillonBusinessException {
+            try {
+                myDO.setLang(lang);
+            } catch(DataObjectException ex) {
+                throw new PapillonBusinessException("Error setting index lang", ex);
+            }
+        }
+
+    /**
+		* Gets the value of the key
+     *
+     * @return the value as a String.
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+						  *   error).
+     */
+	
+    /**
+     * Gets the EntryId
+	 *
+     * @return the entry id.
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+	 *   error).
+     */
+	public int getEntryId()
+        throws PapillonBusinessException {
+			try {
+				return myDO.getEntryId();
+			} catch(DataObjectException ex) {
+				throw new PapillonBusinessException("Error getting index's entry id", ex);
+			}
+		}
+
+    /**
+	 * Sets the entry id
+     *
+     * @param the entry id.
+     * @exception PapillonBusinessException if an error occurs
+     *   retrieving data (usually due to an underlying data layer
+	 *   error).
+     */
+     public void setEntryId(int entryId)
+        throws PapillonBusinessException {
+            try {
+                myDO.setEntryId(entryId);
+            } catch(DataObjectException ex) {
+                throw new PapillonBusinessException("Error setting index's entry id", ex);
+            }
+        }
+
+    /**
+        * Saves the index to the database.
+     *
+     * @exception PapillonBusinessException if an error occurs
+     *   deleting data (usually due to an underlying data layer
+                        *   error).
+     */
+    public void save() 
+        throws PapillonBusinessException {
+        try {
+            this.myDO.commit();
+        } catch(Exception ex) {
+            throw new PapillonBusinessException("Error saving index", ex);
+        }
+    }
+    
+    /**
+     * Deletes the volume entry from the database.
+     *
+     * @exception PapillonBusinessException if an error occurs
+     *   deleting data (usually due to an underlying data layer
+     *   error).
+     */
+    public void delete() 
+        throws PapillonBusinessException {
+        try {
+            this.myDO.delete();
+        } catch(Exception ex) {
+            throw new PapillonBusinessException("Error deleting index", ex);
+        }
+    }
+}
---
+++ src/fr/imag/clips/papillon/business/dictionary/LinkFactory.java
@@ -0,0 +1,304 @@
+/*
+ -----------------------------
+ * $Id: LinkFactory.java 1111 2012-03-26 22:00:04 zhang ying $
+ *-----------------------------------------------
+ * $Log$
+ * 
+ *-----------------------------------------------
+ *
+ */
+
+package fr.imag.clips.papillon.business.dictionary;
+
+import com.lutris.dods.builder.generator.query.QueryBuilder;
+import fr.imag.clips.papillon.CurrentDBTransaction;
+import fr.imag.clips.papillon.business.PapillonBusinessException;
+import fr.imag.clips.papillon.business.PapillonLogger;
+import fr.imag.clips.papillon.data.LinkDO;
+import fr.imag.clips.papillon.data.LinkQuery;
+import fr.imag.clips.papillon.papillon_data.ManageDatabase;
+
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+* Used to find the instances of xslsheet.
+ */
+public class LinkFactory {
+	
+	public final static String LINK_TABLE_PREFIX = "link";
+	
+	public final static String OBJECTID_FIELD = "objectid";
+
+	public final static String ENTRYID_FIELD = "entryid";
+
+	
+	public final static String ORDER_ASCENDING = "";
+	public final static String ORDER_DESCENDING = "DESC";
+	
+	protected final static String databaseName = LinkDO.get_logicalDBName(); 
+	public static String databaseVendor = null;
+	protected static com.lutris.dods.builder.generator.query.RDBColumn[] Columns = new com.lutris.dods.builder.generator.query.RDBColumn[1];
+
+	static {
+		try  {
+			databaseVendor = org.enhydra.dods.DODS.getDatabaseManager().logicalDatabaseType(databaseName);
+		}
+		catch (com.lutris.appserver.server.sql.DatabaseManagerException ex) {
+			databaseVendor = null;
+		}
+		catch (java.sql.SQLException ex) {
+			databaseVendor = null;
+		}
+	}
+		
+	
+        
+	public static Vector getLinkVectorByEntryId(Volume volume, String entryId) throws PapillonBusinessException {
+		Vector theLink = new Vector();
+		
+		if (entryId != null && !entryId.equals("")) {
+			int intId = 0;
+			try {
+				intId = Integer.parseInt(entryId);
+			}
+			catch(Exception ex) {
+				throw new PapillonBusinessException("Exception in getLinkVectorByEntryId()", ex);
+			}
+			
+			try {
+				LinkQuery query = new LinkQuery(volume.getLinkDbname(), CurrentDBTransaction.get());
+				query.setQueryEntryId(intId);
+				LinkDO[] DOarray = query.getDOArray();
+				if (null != DOarray) {
+					for (int j=0; j < DOarray.length; j++) {
+						theLink.add(new Link(DOarray[j]));
+					}
+				}
+			}
+			catch(Exception ex) {
+				throw new PapillonBusinessException("Exception in getLinkVectorByEntryId()", ex);
+			}
+		}
+		return theLink;
+	}
+	
+/*////	public static Vector getLinkEntriesVector(String linkTableName, Vector Keys, String order, int limit) throws PapillonBusinessException {
+		return getLinkEntriesVector(linkTableName, Keys, order, limit,0);
+	}
+////*/
+	
+/*////	public static Vector getLinkEntriesVector(String linkTableName, Vector Keys, String order, int limit, int offset) throws PapillonBusinessException {
+        Vector theEntries = new Vector();
+		
+		if (null != linkTableName) {
+			try {
+			////	com.lutris.dods.builder.generator.query.RDBColumn keyColumn = LinkDO.getKeyColumn(linkTableName);
+				com.lutris.dods.builder.generator.query.RDBColumn langColumn = LinkDO.getLangColumn(linkTableName);
+			////	com.lutris.dods.builder.generator.query.RDBColumn valueColumn = LinkDO.getValueColumn(linkTableName);
+				LinkQuery query = new LinkQuery(linkTableName, CurrentDBTransaction.get());
+				//fr.imag.clips.papillon.business.PapillonLogger.writeDebugMsg("Index request table: " + indexTableName);
+	
+				if (Keys != null) {
+					for (java.util.Enumeration enumKeys = Keys.elements(); enumKeys.hasMoreElements();) {
+						String[] key = (String[]) enumKeys.nextElement();
+						if (key!=null && key[2] !=null && !key[2].equals("")) {
+						////	query.getQueryBuilder().addWhere(keyColumn, key[0], QueryBuilder.EQUAL);
+							if (key[1] !=null && !key[1].equals("")) {
+								query.getQueryBuilder().addWhere(langColumn, key[1], QueryBuilder.EQUAL);
+							}
+							if ( key[3] == QueryBuilder.LESS_THAN ||
+								 key[3] == QueryBuilder.LESS_THAN_OR_EQUAL ||
+								 key[3] == QueryBuilder.GREATER_THAN ||
+								 key[3] == QueryBuilder.GREATER_THAN_OR_EQUAL) {
+								query.getQueryBuilder().addWhere(MSORT_FIELD + key[3]+ "multilingual_sort('" + key[1] + "','" + key[2] + "')");
+							}
+							else {
+						////		query.getQueryBuilder().addWhere(valueColumn, key[2],  key[3]);
+							}
+						}
+					}
+				}				
+				query.getQueryBuilder().setMaxRows((0 == limit) ? DictionariesFactory.MaxRetrievedEntries : limit);
+				if (offset!=0) {
+					query.getQueryBuilder().addEndClause("OFFSET " + offset);
+				}
+				if (order==null || !order.equals(ORDER_DESCENDING)) {
+					order = "";
+				}
+				query.getQueryBuilder().addOrderByColumn(MSORT_FIELD,order);
+				// debug
+				//query.getQueryBuilder().debug();
+				
+				LinkDO[] DOarray = query.getDOArray();
+				if (null != DOarray) {
+					for (int j=0; j < DOarray.length; j++) {
+						Link tempLink = new Link(DOarray[j]);
+						theEntries.add(tempLink);
+					}
+				}
+			}
+			catch(Exception ex) {
+				throw new PapillonBusinessException("Exception in getLinkEntriesVector()", ex);
+			}
+		}
+		return theEntries;
+	}
+	////*/
+	
+	protected static boolean createLinkForLexiesHashtable(String linkDbname, Axie myAxie)
+		throws PapillonBusinessException {
+			Hashtable lexies = myAxie.getLexies();
+			if (lexies != null) {
+				Iterator myIterator = lexies.values().iterator();
+				while (myIterator.hasNext()) {
+					Vector myVector = (Vector) myIterator.next();
+					if (myVector != null && myVector.size()>0) {
+						for (int i=0;i<myVector.size();i++) {
+							String myString = (String) myVector.elementAt(i);
+							if (myString != null && !myString.equals("")) {
+								////Link myLink = LinkFactory.newLink(linkDbname, Volume.CDM_entryId, Axie.LANG,myString,myAxie.getHandle());
+								////myLink.save();
+							}
+						}
+					}
+				}				
+			}
+			return true;
+		}	
+	
+	
+	protected static void deleteLinkForEntryId(String linkDbname, String entryId) throws 	PapillonBusinessException {
+		Vector theLink = new Vector();
+		
+		String cmp_op = QueryBuilder.EQUAL;
+		
+		int intId = 0;
+		try {
+			intId = Integer.parseInt(entryId);
+		}
+		catch(Exception ex) {
+			throw new PapillonBusinessException("Exception in newLink()", ex);
+		}
+		
+		
+		if (entryId != null && !entryId.equals("")) {
+			try {
+				LinkQuery query = new LinkQuery(linkDbname, CurrentDBTransaction.get());
+				query.setQueryEntryId(intId);
+				LinkDO[] DOarray = query.getDOArray();
+				if (null != DOarray) {
+					for (int j=0; j < DOarray.length; j++) {
+						Link myLink = new Link(DOarray[j]);
+						myLink.delete();
+					}
+				}
+			}
+			catch(Exception ex) {
+				throw new PapillonBusinessException("Exception in deleteLinkForEntryId()", ex);
+			}
+		}
+	}
+	
+	public static Link newLink(String table, String targetId, String lang, String handle)
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			//
+			int intId = 0;
+			try {
+				intId = Integer.parseInt(handle);
+			}
+			catch(Exception ex) {
+				throw new PapillonBusinessException("Exception in newLink()", ex);
+			}
+			
+			Link newLink = null;
+			if (targetId != null) {
+				targetId = targetId.trim();
+				if (!targetId.equals("")) {
+					newLink=new Link(table);
+					// lang of the key
+					if (lang.length()==3) {
+						newLink.setLang(lang);
+					}
+					else {
+						newLink.setLang(Volume.DEFAULT_LANG);
+					}
+					// value of the key
+					newLink.setTargetId(targetId);
+					// entry id
+					newLink.setEntryId(intId);
+				}
+			}
+			//fr.imag.clips.papillon.business.PapillonLogger.writeDebugMsg("NEW INDEX : table=" + table + " key=" + key + " lang=" + lang + " value=" + value + " lang=" + handle);
+				return newLink;
+            
+		}
+
+	
+	
+	public static void emptyLink(String table)
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			try {
+				ManageDatabase.truncateTable(table);
+			}
+			catch (Exception e) {
+				throw new fr.imag.clips.papillon.business.PapillonBusinessException ("Exception in emptyLink with table: " + table, e);
+			}
+		}
+	
+	public static void createLinkTable(Volume volume)
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			try {
+				ManageDatabase.createLinkTable(volume.getLinkDbname());
+				createIndexForTable(volume.getLinkDbname());
+			}
+			catch (Exception e) {
+				throw new fr.imag.clips.papillon.business.PapillonBusinessException ("Exception in createLinkTable " + volume.getLinkDbname() + " for volume: " + volume.getName(), e);
+			}
+		}
+	
+	protected static void createIndexForTable(String table)
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			try {
+				ManageDatabase.createIndexForTable(table,OBJECTID_FIELD);
+				ManageDatabase.createIndexForTable(table,ENTRYID_FIELD);
+
+			}
+			catch (Exception e) {
+				throw new fr.imag.clips.papillon.business.PapillonBusinessException ("Exception in createLinkForTable: " + table, e);
+			}
+		}
+	
+	public static void dropLinkTable(Volume theVolume) 
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			dropLinkTable(theVolume.getLinkDbname());
+		}
+	
+	public static void dropLinkTable(String table)
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			try {
+				ManageDatabase.dropTable(table);
+			}
+			catch (Exception e) {
+				throw new fr.imag.clips.papillon.business.PapillonBusinessException ("Exception in dropLinkTable: " + table, e);
+			}
+		}
+	
+	public static void truncateLinkTable(Volume volume) 
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			truncateLinkTable(volume.getLinkDbname());
+		}
+	
+	public static void truncateLinkTable(String table)
+		throws fr.imag.clips.papillon.business.PapillonBusinessException {
+			try {
+				ManageDatabase.truncateTable(table);
+			}
+			catch (Exception e) {
+				throw new fr.imag.clips.papillon.business.PapillonBusinessException ("Exception in truncateLinkTable: " + table, e);
+			}
+		}
+	}
+
