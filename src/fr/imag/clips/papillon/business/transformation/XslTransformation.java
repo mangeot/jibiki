b7a9a323e308a9bcc483a17fc4cbddb7bf321916
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2013-01-07 10:48:12 +0000

    Adding a debug message when no XSL sheet is available for displaying a query result
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/LINKS_1_0@1404 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: 52a098e33d08ccd59ffaffd44d00da03acae6c85

--- src/fr/imag/clips/papillon/business/transformation/XslTransformation.java
+++ src/fr/imag/clips/papillon/business/transformation/XslTransformation.java
@@ -343,11 +343,16 @@ public class XslTransformation implements ResultFormatter {
 				resultNode = myAnswer.getDom().getDocumentElement();
 				
 			}
-			//PapillonLogger.writeDebugMsg("ResultNode: " + qr.getSourceEntry().getHeadword() + " node: " + XMLServices.NodeToString(resultNode)+", "+resultNode.getNodeValue());
+			PapillonLogger.writeDebugMsg("ResultNode: " + qr.getSourceEntry().getHeadword() + " node: " + XMLServices.NodeToString(resultNode)+", "+resultNode.getNodeValue());
 				
 				rootdiv.appendChild(res.importNode(resultNode, true));
 					//PapillonLogger.writeDebugMsg("rootdiv:"+rootdiv.getTextContent());
 			//}				
+		}
+		else {
+			PapillonLogger.writeDebugMsg("XSL sheet empty for dict "+ qr.getSourceEntry().getDictionaryName()+"!");
+			Node resultNode = myAnswer.getDom().getDocumentElement();
+			rootdiv.appendChild(res.importNode(resultNode, true));
 		}
 		return rootdiv;
 	} 
