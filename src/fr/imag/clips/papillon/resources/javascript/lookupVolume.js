9de7509ee9c02e2134a1477814a9fc33fd3380b9
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2015-03-11 00:39:30 +0000

    appel du javascript au chargement de la page
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/LINKS_1_0@1595 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: 841cb32d74f2d54044c50b0abd4d2e02a9604095

--- src/fr/imag/clips/papillon/resources/javascript/lookupVolume.js
+++ src/fr/imag/clips/papillon/resources/javascript/lookupVolume.js
@@ -243,4 +243,19 @@ function replace_api(string) {
 	return string;
 }
 
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+
+
 // -->
