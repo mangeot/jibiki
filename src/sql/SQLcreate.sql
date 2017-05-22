99c708459c49e146119cec97850cc80611119c78
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2017-05-22 20:08:46 +0000

    Modificaiton de la base de données
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/LINKS_1_0@1824 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: cce3e11cc042a52cd51aa8fb6ec62e8ff2690912

--- src/sql/SQLcreate.sql
+++ src/sql/SQLcreate.sql
@@ -1,5 +1,140 @@
 
 
+create table contributionlog 
+(
+/* class ContributionLog*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    author VARCHAR (255) NOT NULL ,
+    groups VARCHAR (255) NOT NULL ,
+    volume VARCHAR (255) NOT NULL ,
+    sourceLanguage VARCHAR (255) NOT NULL ,
+    headword VARCHAR (255) NOT NULL ,
+    contributionId VARCHAR (255) NOT NULL ,
+    entryId VARCHAR (255) NOT NULL ,
+    date TIMESTAMP NOT NULL ,
+    status VARCHAR (255) NOT NULL
+);
+
+
+CREATE INDEX contributionlog_author_idx ON contributionlog (author);
+CREATE INDEX contributionlog_volume_idx ON contributionlog (volume);
+CREATE INDEX contributionlog_headword_idx ON contributionlog (headword);
+CREATE INDEX contributionlog_contributionid_idx ON contributionlog (contributionId);
+CREATE INDEX contributionlog_entryid_idx ON contributionlog (entryId);
+CREATE INDEX contributionlog_status_idx ON contributionlog (status);
+
+
+
+
+create table dictionaries 
+(
+/* class Dictionary*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    fullName VARCHAR (255) NOT NULL ,
+    name VARCHAR (255) NOT NULL ,
+    category VARCHAR (255) NOT NULL ,
+    type VARCHAR (255) NOT NULL ,
+    domain VARCHAR (255) NOT NULL ,
+    legal VARCHAR (255) NOT NULL ,
+    access VARCHAR (255) NOT NULL ,
+    sourceLanguages VARCHAR (255) NOT NULL ,
+    targetLanguages VARCHAR (255) NOT NULL ,
+    xmlCode TEXT NOT NULL
+);
+
+
+CREATE UNIQUE INDEX dictionaries_fullname_idx ON dictionaries (fullname);
+CREATE UNIQUE INDEX dictionaries_name_idx ON dictionaries (name);
+
+
+
+
+create table foksentry 
+(
+/* class FoksEntry*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    headword VARCHAR (255) ,
+    reading VARCHAR (255) ,
+    id INTEGER ,
+    score REAL
+);
+
+
+CREATE INDEX foksentry_reading_idx ON foksentry (reading);
+
+
+
+
+create table groups 
+(
+/* class Group*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    name VARCHAR (255) NOT NULL ,
+    password BYTEA NOT NULL ,
+    users TEXT ,
+    admins TEXT
+);
+
+
+CREATE UNIQUE INDEX groups_name_idx ON groups (name);
+
+
+
+
+create table informationdocument 
+(
+/* class InformationDocument*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    author TEXT ,
+    owner VARCHAR (255) NOT NULL ,
+    reference TEXT ,
+    creationDate DATE NOT NULL ,
+    section VARCHAR (255) ,
+    title TEXT
+);
+
+
+
+
+
+
+create table informationfiles 
+(
+/* class InformationFile*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    filename TEXT NOT NULL ,
+    filetype TEXT NOT NULL ,
+    document DECIMAL (19,0) NOT NULL ,
+    filecode TEXT ,
+    isIndexFile CHAR (1) NOT NULL ,
+    language VARCHAR (5) NOT NULL ,    
+    CONSTRAINT informationfiles_document FOREIGN KEY (document) REFERENCES informationdocument (ObjectId)
+);
+
+
+
+
+
+
+create table jibikiversion 
+(
+/* class JibikiVersion*/
+    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
+    ObjectVersion INTEGER  NOT NULL ,
+    number INTEGER NOT NULL
+);
+
+
+
+
+
+
 create table messages 
 (
 /* class Message*/
@@ -47,29 +182,26 @@ CREATE INDEX querylogs_queryString_idx ON querylogs (queryString);
 
 
 
-create table contributionlog 
+create table users 
 (
-/* class ContributionLog*/
+/* class User*/
     ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
     ObjectVersion INTEGER  NOT NULL ,
-    author VARCHAR (255) NOT NULL ,
-    groups VARCHAR (255) NOT NULL ,
-    volume VARCHAR (255) NOT NULL ,
-    sourceLanguage VARCHAR (255) NOT NULL ,
-    headword VARCHAR (255) NOT NULL ,
-    contributionId VARCHAR (255) NOT NULL ,
-    entryId VARCHAR (255) NOT NULL ,
-    date TIMESTAMP NOT NULL ,
-    status VARCHAR (255) NOT NULL
+    name VARCHAR (255) NOT NULL ,
+    login VARCHAR (255) NOT NULL ,
+    password VARCHAR (255) NOT NULL ,
+    email VARCHAR (255) NOT NULL ,
+    lang VARCHAR (3) ,
+    groups VARCHAR (255) ,
+    credits INTEGER NOT NULL ,
+    xmlCode TEXT ,
+    creationDate DATE ,
+    modificationDate DATE
 );
 
 
-CREATE INDEX contributionlog_author_idx ON contributionlog (author);
-CREATE INDEX contributionlog_volume_idx ON contributionlog (volume);
-CREATE INDEX contributionlog_headword_idx ON contributionlog (headword);
-CREATE INDEX contributionlog_contributionid_idx ON contributionlog (contributionId);
-CREATE INDEX contributionlog_entryid_idx ON contributionlog (entryId);
-CREATE INDEX contributionlog_status_idx ON contributionlog (status);
+CREATE UNIQUE INDEX users_name_idx ON users (name);
+CREATE UNIQUE INDEX users_login_idx ON users (login);
 
 
 
@@ -102,123 +234,6 @@ CREATE INDEX volumes_dbname_idx ON volumes (dbname);
 
 
 
-create table informationdocument 
-(
-/* class InformationDocument*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    author TEXT ,
-    owner VARCHAR (255) NOT NULL ,
-    reference TEXT ,
-    creationDate DATE NOT NULL ,
-    section VARCHAR (255) ,
-    title TEXT
-);
-
-
-
-
-
-
-create table users 
-(
-/* class User*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    name VARCHAR (255) NOT NULL ,
-    login VARCHAR (255) NOT NULL ,
-    password BYTEA NOT NULL ,
-    email VARCHAR (255) NOT NULL ,
-    lang VARCHAR (3) ,
-    groups VARCHAR (255) ,
-    credits INTEGER NOT NULL ,
-    xmlCode TEXT
-);
-
-
-CREATE UNIQUE INDEX users_name_idx ON users (name);
-CREATE UNIQUE INDEX users_login_idx ON users (login);
-
-
-
-
-create table dictionaries 
-(
-/* class Dictionary*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    fullName VARCHAR (255) NOT NULL ,
-    name VARCHAR (255) NOT NULL ,
-    category VARCHAR (255) NOT NULL ,
-    type VARCHAR (255) NOT NULL ,
-    domain VARCHAR (255) NOT NULL ,
-    legal VARCHAR (255) NOT NULL ,
-    sourceLanguages VARCHAR (255) NOT NULL ,
-    targetLanguages VARCHAR (255) NOT NULL ,
-    xmlCode TEXT NOT NULL
-);
-
-
-CREATE UNIQUE INDEX dictionaries_fullname_idx ON dictionaries (fullname);
-CREATE UNIQUE INDEX dictionaries_name_idx ON dictionaries (name);
-
-
-
-
-create table papillonaxi 
-(
-/* class PapillonAxi*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    id VARCHAR (255) ,
-    semanticCat VARCHAR (255) ,
-    dom BYTEA NOT NULL ,
-    htmldom BYTEA ,
-    xmlCode TEXT NOT NULL ,
-    lexies BYTEA NOT NULL ,
-    synonyms VARCHAR (255)
-);
-
-
-CREATE INDEX papillonaxi_id_idx ON papillonaxi (id);
-
-
-
-
-create table foksentry 
-(
-/* class FoksEntry*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    headword VARCHAR (255) ,
-    reading VARCHAR (255) ,
-    id INTEGER ,
-    score REAL
-);
-
-
-CREATE INDEX foksentry_reading_idx ON foksentry (reading);
-
-
-
-
-create table groups 
-(
-/* class Group*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    name VARCHAR (255) NOT NULL ,
-    password BYTEA NOT NULL ,
-    users TEXT ,
-    admins TEXT
-);
-
-
-CREATE UNIQUE INDEX groups_name_idx ON groups (name);
-
-
-
-
 create table xslsheets 
 (
 /* class XslSheet*/
@@ -231,38 +246,6 @@ create table xslsheets
     code TEXT NOT NULL ,
     defaultxsl CHAR (1) NOT NULL ,
     externalxsl CHAR (1) NOT NULL
-);
-
-
-
-
-
-
-create table jibikiversion 
-(
-/* class JibikiVersion*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    number INTEGER NOT NULL
-);
-
-
-
-
-
-
-create table informationfiles 
-(
-/* class InformationFile*/
-    ObjectId DECIMAL (19,0) NOT NULL PRIMARY KEY ,
-    ObjectVersion INTEGER  NOT NULL ,
-    filename TEXT NOT NULL ,
-    filetype TEXT NOT NULL ,
-    document DECIMAL (19,0) NOT NULL ,
-    filecode TEXT ,
-    isIndexFile CHAR (1) NOT NULL ,
-    language VARCHAR (5) NOT NULL ,    
-    CONSTRAINT informationfiles_document FOREIGN KEY (document) REFERENCES informationdocument (ObjectId)
 );
 
 
