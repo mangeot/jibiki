066a5da0dbccdcbda124c5847a4fc4ff28e340e3
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2017-05-24 14:16:07 +0000

    Added WORKDIR variable to call the script from elsewhere
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/LINKS_1_0@1835 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: 4c5e07be00ae50ff98af42a07858116608f94414

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-sed -i "s#\%ADMIN_PASSWORD\%#$ADMIN_PASSWORD#g" papillon.properties
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sed -i "s#\%SPECIALIST_PASSWORD\%#$SPECIALIST_PASSWORD#g" papillon.properties
+sed -i "s#\%ADMIN_PASSWORD\%#$ADMIN_PASSWORD#g" $WORKDIR/papillon.properties
 
-sed -i "s#\%DATABASE_HOST\%#$DATABASE_HOST#g" papillon.properties
+sed -i "s#\%SPECIALIST_PASSWORD\%#$SPECIALIST_PASSWORD#g" $WORKDIR/papillon.properties
 
-sed -i "s#\%DATABASE_NAME\%#$DATABASE_NAME#g" papillon.properties
+sed -i "s#\%DATABASE_HOST\%#$DATABASE_HOST#g" $WORKDIR/papillon.properties
 
-sed -i "s#\%DATABASE_USER\%#$DATABASE_USER#g" papillon.properties
+sed -i "s#\%DATABASE_NAME\%#$DATABASE_NAME#g" $WORKDIR/papillon.properties
 
-sed -i "s#\%DATABASE_PASSWORD\%#$DATABASE_PASSWORD#g" papillon.properties
+sed -i "s#\%DATABASE_USER\%#$DATABASE_USER#g" $WORKDIR/papillon.properties
+
+sed -i "s#\%DATABASE_PASSWORD\%#$DATABASE_PASSWORD#g" $WORKDIR/papillon.properties
 
 output/run --debug --exec
