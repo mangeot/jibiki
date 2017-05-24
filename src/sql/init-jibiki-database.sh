a5f771b58e3e3d8d74664d2b55ce8c298b4001a6
mangeot <mangeot@73f06212-78d2-4448-adb7-01a149c5d412>
2017-05-24 12:29:59 +0000

    ajout d'un script pour initlaiser la bd
    git-svn-id: https://svn.ligforge.imag.fr/svn/jibiki/branches/LINKS_1_0@1826 73f06212-78d2-4448-adb7-01a149c5d412
    
    Former-commit-id: 5da5c63b5af29875a59f852ade0c12735dce502f

---
+++ src/sql/init-jibiki-database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWORD' CREATEDB;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$DATABASE_USER" <<-EOSQL
+    CREATE DATABASE $DATABASE_NAME WITH ENCODING 'UTF-8';
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $DATABASE_NAME $DATABASE_USER -f sql/multilingual-sort.sql
+
+psql -v ON_ERROR_STOP=1 --username $DATABASE_NAME $DATABASE_USER -f sql/SQLcreate.sql
+
